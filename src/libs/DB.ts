b3b95d0569df56244687c5279427f0d29ea578e6
jtrussell10 <69161168+jtrussell10@users.noreply.github.com>
2024-07-22 12:51:01 +0100

    chore: attempted fix 2001

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -1,46 +1,41 @@
-// // import { PGlite } from '@electric-sql/pglite';
+import { PGlite } from '@electric-sql/pglite';
 import { drizzle as drizzlePg } from 'drizzle-orm/node-postgres';
 import { migrate as migratePg } from 'drizzle-orm/node-postgres/migrator';
-// import type { PgDatabase } from 'drizzle-orm/pg-core';
-// import { drizzle as drizzlePglite } from 'drizzle-orm/pglite';
-// import { migrate as migratePglite } from 'drizzle-orm/pglite/migrator';
-// import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants';
+import type { PgDatabase } from 'drizzle-orm/pg-core';
+import { drizzle as drizzlePglite } from 'drizzle-orm/pglite';
+import { migrate as migratePglite } from 'drizzle-orm/pglite/migrator';
+import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants';
 import { Client } from 'pg';
 
 import * as schema from '@/models/Schema';
 
 import { Env } from './Env';
 
-// if (
-//   process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD &&
-//   process.env.NODE_ENV === 'production' &&
-//   Env.DATABASE_URL
-// ) {
-//   client = new Client({
-//     connectionString: Env.DATABASE_URL,
-//   });
-//   await client.connect();
+let client;
+let drizzle: PgDatabase<any, any, any>;
 
-//   drizzle = drizzlePg(client, { schema });
-//   await migratePg(drizzle, { migrationsFolder: './migrations' });
-// } else {
-//   const global = globalThis as unknown as { client: PGlite };
+if (
+  process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD &&
+  process.env.NODE_ENV === 'production' &&
+  Env.DATABASE_URL
+) {
+  client = new Client({
+    connectionString: Env.DATABASE_URL,
+  });
+  await client.connect();
 
-//   if (!global.client) {
-//     global.client = new PGlite();
-//     await global.client.waitReady;
-//   }
+  drizzle = drizzlePg(client, { schema });
+  await migratePg(drizzle, { migrationsFolder: './migrations' });
+} else {
+  const global = globalThis as unknown as { client: PGlite };
 
-//   drizzle = drizzlePglite(global.client, { schema });
-//   await migratePglite(drizzle, { migrationsFolder: './migrations' });
-// }
+  if (!global.client) {
+    global.client = new PGlite();
+    await global.client.waitReady;
+  }
 
-const client = new Client({
-  connectionString: Env.DATABASE_URL,
-});
-await client.connect();
-
-const drizzle = drizzlePg(client, { schema });
-await migratePg(drizzle, { migrationsFolder: './migrations' });
+  drizzle = drizzlePglite(global.client, { schema });
+  await migratePglite(drizzle, { migrationsFolder: './migrations' });
+}
 
 export const db = drizzle;
