5e2a246412d1d6083b5d2d51db7e8a4ea9f826b6
jtrussell10 <69161168+jtrussell10@users.noreply.github.com>
2024-07-24 21:36:25 +0100

    chore: github actions attempted fix

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -12,7 +12,7 @@ const Hello = async () => {
         {t('hello_message', { email: user?.emailAddresses[0]?.emailAddress })}
       </p>
       <p>
-        Looking for a SaaS Boilerplate?{' '}
+        Looking for a SaaS Boilerplate? TestTestTest{' '}
         <a
           className="text-blue-700 hover:border-b-2 hover:border-blue-700"
           href="https://nextjs-boilerplate.com/pro-saas-starter-kit"
