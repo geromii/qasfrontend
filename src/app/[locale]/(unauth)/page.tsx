e6f6745e2d634d8d0fb1daa3696c22b314bfaea5
jtrussell10 <69161168+jtrussell10@users.noreply.github.com>
2024-07-25 14:30:46 +0100

    chore: minor fix

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -85,7 +85,7 @@ export default function Index() {
           Better Stack
         </a>
         , Monitoring as Code with Checkly, Storybook, Multi-language (i18n), and
-        more.
+        more. Test.
       </p>
     </>
   );
